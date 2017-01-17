8034684f89823c6608bcdc4292bda05027e573ad
Ben Kirzhner <ben.kirzhner@gmail.com>
2017-01-17 00:30:27 -0800

    Solve q2 part 2

--- puzzle_2.exs
+++ puzzle_2.exs
@@ -1,19 +1,8 @@
 defmodule AdventOfCode.Puzzle2 do
-
-  ExUnit.start
-
-  @keypad {
-    {1, 2, 3},
-    {4, 5, 6},
-    {7, 8, 9},
-  }
-
-  @starting_position {1, 1}
-
-  def calculate_code(instructions) do
+  def calculate_code(instructions, keypad, starting_position) do
     instructions
     |> parse_instructions
-    |> do_calculate_code
+    |> do_calculate_code(keypad, starting_position)
   end
 
   defp parse_instructions(instructions_string) do
@@ -32,13 +21,19 @@ defmodule AdventOfCode.Puzzle2 do
   defp instruction_for_char("L"), do: :left
   defp instruction_for_char("R"), do: :right
 
-  defp do_calculate_code(instruction_sequences) do
+  defp do_calculate_code(instruction_sequences, keypad, starting_position) do
+    # This assumes a square keypad.
+    keypad_size = tuple_size(keypad)
+
     {_, keypresses} =
       instruction_sequences
-      |> Enum.reduce({@starting_position, []}, fn instruction_sequence, {position, keypresses} ->
-           new_position = Enum.reduce(instruction_sequence, position, &apply_instruction/2)
+      |> Enum.reduce({starting_position, []}, fn instruction_sequence, {position, keypresses} ->
+           new_position = Enum.reduce(instruction_sequence, position, fn instruction, current_position ->
+             new_position = apply_instruction(instruction, current_position, keypad_size)
+             if keypress(keypad, new_position), do: new_position, else: current_position
+           end)
 
-           {new_position, [keypress(new_position) | keypresses]}
+           {new_position, [keypress(keypad, new_position) | keypresses]}
          end)
 
     keypresses
@@ -46,31 +41,53 @@ defmodule AdventOfCode.Puzzle2 do
     |> Enum.join("")
   end
 
-  defp apply_instruction(:up, {x, y}), do:    {max(x - 1, 0), y}
-  defp apply_instruction(:down, {x, y}), do:  {min(x + 1, 2), y}
-  defp apply_instruction(:left, {x, y}), do:  {x, max(y - 1, 0)}
-  defp apply_instruction(:right, {x, y}), do: {x, min(y + 1, 2)}
+  defp apply_instruction(:up, {r, c}, _), do:       {max(r - 1, 0), c}
+  defp apply_instruction(:down, {r, c}, size), do:  {min(r + 1, size - 1), c}
+  defp apply_instruction(:left, {r, c}, _), do:     {r, max(c - 1, 0)}
+  defp apply_instruction(:right, {r, c}, size), do: {r, min(c + 1, size - 1)}
 
-  defp keypress({x, y}), do: @keypad |> elem(x) |> elem(y)
+  defp keypress(keypad, {r, c}), do: keypad |> elem(r) |> elem(c)
+
+  ExUnit.start
 
   defmodule Test do
     use ExUnit.Case
 
     @instructions File.read!("puzzle_2.input")
 
-    test "first half sample" do
-      sample_instructions = """
-      ULL
-      RRDDD
-      LURDL
-      UUUUD
-      """
-      assert AdventOfCode.Puzzle2.calculate_code(sample_instructions) == "1985"
-    end
+    @sample_instructions """
+    ULL
+    RRDDD
+    LURDL
+    UUUUD
+    """
 
     test "first half" do
-      IO.inspect AdventOfCode.Puzzle2.calculate_code(@instructions)
-      assert false
+      keypad = {
+        {"1", "2", "3"},
+        {"4", "5", "6"},
+        {"7", "8", "9"},
+      }
+
+      starting_position = {1, 1}
+
+      assert AdventOfCode.Puzzle2.calculate_code(@sample_instructions, keypad, starting_position) == "1985"
+      assert AdventOfCode.Puzzle2.calculate_code(@instructions, keypad, starting_position) == "74921"
+    end
+
+    test "second half" do
+      keypad = {
+        {nil, nil, "1", nil, nil},
+        {nil, "2", "3", "4", nil},
+        {"5", "6", "7", "8", "9"},
+        {nil, "A", "B", "C", nil},
+        {nil, nil, "D", nil, nil},
+      }
+
+      starting_position = {2, 0}
+
+      assert AdventOfCode.Puzzle2.calculate_code(@sample_instructions, keypad, starting_position) == "5DB3"
+      IO.inspect AdventOfCode.Puzzle2.calculate_code(@instructions, keypad, starting_position), label: "answer"
     end
   end
 end
